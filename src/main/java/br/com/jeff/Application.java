c1e774e697f3208e9edf27ac0748a2d10d8d1d8d
jeffnunes <jef_nunes@outlook.com>
2025-02-14 16:26:10 -0300

    aplicação de acesso a um banco de dados de livros

--- src/main/java/br/com/jeff/Application.java
+++ src/main/java/br/com/jeff/Application.java
@@ -2,8 +2,10 @@ package br.com.jeff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class Application {
---
+++ src/main/java/br/com/jeff/controller/FooBarController.java
@@ -0,0 +1,27 @@
+package br.com.jeff.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+
+    private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
+
+    @GetMapping("/foo-bar")
+    @Retry(name = "default")
+    public String FooBar(){
+        logger.info("request to foo bar is received!");
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8080/foo-bar", String.class);
+        //return "FooBar!!!";
+        return response.getBody();
+    }
+}
